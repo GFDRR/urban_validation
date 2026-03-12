824677555b37a1f799b41df55509ff320388f5a6
Rufai Omowunmi Balogun <rufaibalogun1@gmail.com>
2026-03-12 03:54:27 -0400

    [WIP] pseudo code for full validation pipeline

---
+++ main.py
@@ -0,0 +1,6 @@
+## pseudo code
+## load configs from the harmonized version for the given aoi
+## call UrbanVectorDownloader to download all specified vector datasets for that AOI
+## call UrbanRasterDownloader to download all specified raster datasets for that AOI
+## load the respective paths of the download datasets and perform validation based on the datatype (vector vs raster)
+## save the visualizations for the metrics 
